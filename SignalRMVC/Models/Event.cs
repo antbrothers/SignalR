e9d023cc32cd210c05f383f4a067baa96f410230
linjainxi <jianxi_lin@163.com>
2015-11-10 16:48:41 +0800

    模拟静态数据

---
+++ SignalRMVC/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRMVC.Models
+{
+    public class Event
+    {
+        public  int Id { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public DateTime? Time { get; set; }
+    }
+}
---
+++ SignalRMVC/Models/EventRepertory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRMVC.Models
+{
+    public class EventRepertory
+    {
+        private int _nextId = 1;
+        List<Event> events=new List<Event>(); 
+        public EventRepertory()
+        {
+            Add(new Event()
+            {
+                Name = "ant",
+                Desc = "hello word",
+            });
+            Add(new Event()
+            {
+                Name = "ant",
+                Desc = "最新版spring ide for eclipse在线安装方法"
+            });
+        }
+
+        /// <summary>
+        /// 添加元素
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public Event Add(Event model)
+        {
+            if (model!=null)
+            {
+                model.Id = _nextId++;
+                model.Time = DateTime.Now;
+                events.Add(model);
+            }
+            return model;
+        }
+    }
+}
